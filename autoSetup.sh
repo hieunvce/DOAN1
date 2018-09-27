2ffef574f3ed5e1b59b52a4a5fc7edacf8ac5a9b
hieunvce <hieunv.ce@gmail.com>
2018-09-27 05:30:37 +0100

    removed opencv_contrib

--- autoSetup.sh
+++ autoSetup.sh
@@ -1,8 +1,12 @@
-#!bin/bash
+#!/bin/bash
 
-sudo chmod +x installOpenCV.sh
-sudo ./installOpenCV.sh | tee setupLog.txt
-git add installOpenCV.sh autoSetup.sh setupLog.txt
+sudo chmod +x installOpenCv.sh
+
+rm -f setupLog.txt
+echo "Removed setupLog.txt"
+
+sudo ./installOpenCv.sh | tee setupLog.txt
+git add installOpenCv.sh autoSetup.sh setupLog.txt
 git commit -m "Latest setup."
 git push origin master
 
